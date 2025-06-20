9783cefe24210b89bf853f0b4bfe4e66372e7b0a
urban-calderon <urbanocalderon@outlook.com>
2025-06-20 00:52:40 -0600

    feat: add new routes for task services and countries

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes';
+import countryRoutes from './routes/country.routes';
+import taskRoutes from './routes/task.routes';
 import bodyParser from 'body-parser';
 import { errorMiddleware } from './middlerwares/error.middlerware';
 
@@ -10,6 +12,8 @@ export const createApp = () => {
 
     // Routes
     app.use('/api/users', authRoutes);
+    app.use('/api/countries', countryRoutes);
+    app.use('/api/tasks', taskRoutes);
 
     // Error
     app.use(errorMiddleware);
